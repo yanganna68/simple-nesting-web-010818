a97855a09db7d8163b412e3d8ea204b31e238124
ANNA YANG <yanganna68@gmail.com>
2018-01-04 12:50:56 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -119,6 +119,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-	programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+	programmer_hash[:dennis_ritchie][:languages]<<"Java"
 	programmer_hash
 end
