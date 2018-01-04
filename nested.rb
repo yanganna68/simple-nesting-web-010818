ee119e31b375bf6511d2593ee9368272a283d1b1
ANNA YANG <yanganna68@gmail.com>
2018-01-04 12:44:56 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 
 	programmer_hash =
- 		{  
+ 		{
         :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
@@ -27,19 +27,12 @@ end
 def dennis_ritchies_language
 	programmer_hash =
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
