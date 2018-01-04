74864d3cec15112beaf7217c9be7c79295d538c3
ANNA YANG <yanganna68@gmail.com>
2018-01-04 12:44:08 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -5,7 +5,7 @@ def hopper
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
-        },
+        }
      }
 
 programmer_hash[:grace_hopper]
@@ -15,20 +15,13 @@ def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 
 	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
+ 		{  
         :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
