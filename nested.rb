4093a9a1c30fbe7be71dfad57c5e27593d4ffedc
ANNA YANG <yanganna68@gmail.com>
2018-01-04 12:51:28 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -99,7 +99,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-		 programmer_hash[:alan_kay][:known_for]="laugh"
+		 programmer_hash[:alan_kay][:known_for]="GUI"
 		 programmer_hash
 end
 
@@ -119,6 +119,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-	programmer_hash[:dennis_ritchie][:languages]<<"Java"
+	programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
 	programmer_hash
 end
